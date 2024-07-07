0f0f73eae5646fd8f1df5fe3a8e24461c876aeb9
user <user@DESKTOP-TO3VNLO>
2024-07-07 02:01:54 -0700

    data baes code

--- EmployeeHR/Program.cs
+++ EmployeeHR/Program.cs
@@ -1,9 +1,34 @@
+using EmployeeHR.Data;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
+
+
+
+builder.Services.AddControllersWithViews();
+
+
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory
+    .GetCurrentDirectory())
+    .AddJsonFile("appsettings.json").Build();
+
+var connectionString = configuration.GetConnectionString("HRConnectionString");
+
+builder.Services.AddDbContext<HRdbContext>(option => option.UseSqlServer(connectionString));
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
