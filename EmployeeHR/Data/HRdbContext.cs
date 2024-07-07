18c57b73ac9d33ff698d8deea04f2c31bd6e122d
user <user@DESKTOP-TO3VNLO>
2024-07-07 01:00:38 -0700

    db set

--- EmployeeHR/Data/HRdbContext.cs
+++ EmployeeHR/Data/HRdbContext.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EmployeeHR.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeHR.Data
 {
     public class HRdbContext : DbContext
     {
+        public DbSet<EmployeeModel> Employees { get; set; }
 
+        public DbSet<DepartmentModel> Departments { get; set; }
 
     }
 }
